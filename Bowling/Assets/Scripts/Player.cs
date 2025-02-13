8acac99e3f99b50fb4ec0227455f13c49d928140
chrishill93 <chrishill_12@hotmail.com>
2025-02-13 14:46:53 -0800

    Player movement

---
+++ Bowling/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        inputManager.OnMove.AddListener(MovePlayer);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }
+}
