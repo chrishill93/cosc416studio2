ead8ea5d4239dd0ef0653f45f25913336953a14a
chrishill93 <chrishill_12@hotmail.com>
2025-02-15 12:17:56 -0800

    Actually added Reset Functionality

--- Bowling/Assets/Scripts/GameManager.cs
+++ Bowling/Assets/Scripts/GameManager.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputManager;
     [SerializeField] private TextMeshProUGUI scoreText;
-    private FallTrigger[] pins;
+    private FallTrigger[] fallTriggers;
+    private GameObject pinObjects;
 
     private void Start()
     {
-        // This line of code wasn't compiling, and after a quick Google search
-        // it seems that older versions of Unity don't support this "FindObjectsInactive"
-        // without casting it explicitly.
-        pins = FindObjectsByType<FallTrigger>((FindObjectsSortMode)FindObjectsInactive.Include);
-        foreach(FallTrigger pin in pins)
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+    }
+
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins()
+    {
+        if(pinObjects)
+        {
+            foreach (Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
+        }
+
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (FallTrigger pin in fallTriggers)
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
